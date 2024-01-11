587cdbd4536d446c7737aacf605fcb1c62c3bebd
Sam Moore <sam@paradigmpost.com>
2024-01-11 01:13:07 -0500

    Initial Commit

---
+++ cocoaspot/cocoaspot/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  cocoaspot
+//
+//  Created by Travis T. Destiny on 1/11/24.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ cocoaspot/cocoaspot/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  cocoaspot
+//
+//  Created by Travis T. Destiny on 1/11/24.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
